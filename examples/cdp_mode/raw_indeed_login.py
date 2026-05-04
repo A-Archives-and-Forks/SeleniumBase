24f3eed75e49178812f59dae8d387ea5baa71250
Michael Mintz <mdmintz@gmail.com>
2026-05-04 12:30:04 -0400

    Update a CDP Mode example

--- examples/cdp_mode/raw_indeed_login.py
+++ examples/cdp_mode/raw_indeed_login.py
@@ -1,12 +1,12 @@
-"""An example of clicking at custom CAPTCHA coordinates."""
 from seleniumbase import SB
 
 with SB(uc=True, test=True, incognito=True) as sb:
     url = "https://secure.indeed.com/auth"
     sb.activate_cdp_mode(url)
     sb.sleep(2.2)
-    sb.solve_captcha()
-    sb.sleep(2.5)
+    if not sb.is_element_visible('input[type="email"]'):
+        sb.solve_captcha()
+        sb.sleep(2.5)
     sb.type('input[type="email"]', "test@test.com")
     sb.sleep(0.5)
     sb.solve_captcha()
